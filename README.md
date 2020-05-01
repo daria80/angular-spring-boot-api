e1a7bc1004b90246b7c487bd1754c9f9dae62f6d
smirnova <dddsssmmm555@mail.ru>
2020-05-01 17:21:41 +0300

    add crossorigin and run instruction

---
+++ README.md
@@ -0,0 +1,9 @@
+# angular-spring-boot-api
+
+## How to run 
+
+```shell script
+mvn clean install
+
+java -jar target/app-0.0.1-SNAPSHOT.jar
+```
--- src/main/java/com/github/daria80/app/controller/StudentController.java
+++ src/main/java/com/github/daria80/app/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
