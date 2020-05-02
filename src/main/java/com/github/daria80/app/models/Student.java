a70e5a29ca5bb94dce6b33799e1a81056edb742a
smirnova <dddsssmmm555@mail.ru>
2020-05-02 20:02:33 +0300

    add @document

--- src/main/java/com/github/daria80/app/models/Student.java
+++ src/main/java/com/github/daria80/app/models/Student.java
@@ -2,8 +2,10 @@ package com.github.daria80.app.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 public class Student {
 
     @Id
